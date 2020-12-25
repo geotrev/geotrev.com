ff72bc51af45ce6af992d776c914629169d04168
George Treviranus <hello@geotrev.com>
2020-12-24 19:34:20 -0800

    update site a lot

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,20 @@
-;(function() {
+;(function () {
   const endpoint = "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@gwtrev"
+
+  const months = {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December",
+  }
 
   function isPost(item) {
     return item.categories.length
@@ -18,7 +33,7 @@
       })
       .then(data => {
         const cache = localStorage.getItem("posts")
-        const fetchedPosts = JSON.stringify(data.items.filter(isPost).slice(0, 4))
+        const fetchedPosts = JSON.stringify(data.items.filter(isPost).slice(0, 10))
 
         if (!cache || cache !== fetchedPosts) {
           localStorage.setItem("posts", fetchedPosts)
@@ -29,21 +44,6 @@
   }
 
   function getMonth(postMonth) {
-    const months = {
-      0: "January",
-      1: "February",
-      2: "March",
-      3: "April",
-      4: "May",
-      5: "June",
-      6: "July",
-      7: "August",
-      8: "September",
-      9: "October",
-      10: "November",
-      11: "December",
-    }
-
     return months[postMonth]
   }
 
